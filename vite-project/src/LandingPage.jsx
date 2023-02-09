26b0ef5c300cff4e56df9245ade201c47507c4d2
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-02-08 21:34:34 -0500

    Initial commit

---
+++ vite-project/src/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function LandingPage(){
+    return(
+        <div>
+            <h1>Quizzical</h1>
+            <button>Start quiz</button>
+        </div>
+    )
+}
