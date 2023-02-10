a7317b57ccd23c2be4cdc1dc6e39d83de17be39f
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-02-09 20:57:01 -0500

    Continued adjusting styling for landing page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return(
     <main>
       <div className="app-container">
+        <img src="./yellow-blob.png" className="app-topBlob"/>
         <LandingPage/>
       </div>
     </main>
--- vite-project/src/LandingPage.jsx
+++ vite-project/src/LandingPage.jsx
@@ -3,10 +3,9 @@ import React from "react"
 export default function LandingPage(){
     return(
         <div>
-            <img src="./assets/yellow-blob.png"/>
             <h1 className="landingPage-title">Quizzical</h1>
             <button className="landingPage-btn">Start quiz</button>
-            <img src="./assets/blue-blob.png"/>
+            <img src="./blue-blob.png" className="landingPage-bottomBlob"/>
         </div>
     )
 }
