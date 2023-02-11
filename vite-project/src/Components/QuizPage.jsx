26054d53aa06be793f1b28a2817991be6171a29c
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-02-10 22:01:44 -0500

    Quiz page button and blob styling

--- vite-project/src/Components/QuizPage.jsx
+++ vite-project/src/Components/QuizPage.jsx
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function QuizPage(){
     return(
-        <button>Check answers</button>
+        <div>
+            <button className="quizPage-btn">Check answers</button>
+            <img src="./quizPage-blue-blob.png" className="quizPage-bottomBlob"/>
+        </div>
     )
 }
