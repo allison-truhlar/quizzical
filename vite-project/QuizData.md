94248d638fd6aa417fa93950fbd30b56aa1dd14a
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-02-16 17:13:53 -0500

    Fixed bug introduced by state refactoring
    Answer buttons were not showing with new quizData state. Bug causes by square brackets around the questionAndAnswerData

--- vite-project/QuizData.md
+++ vite-project/QuizData.md
@@ -1,6 +1,46 @@
-{
-    answer: "Paul Newman"
-​    answerId: "RRea5Lzebh75eUAeS_eZk"
-​​    isCorrectAnswer: false
-​​​    isSelected: false
-},
+ Object { 
+    question: "Who authored The Adventures of Tintin?", 
+    answers: 
+        { 
+            answerId: "yrdzn-ARPP1a7HZsgEo3c", 
+            answer: "E.P. Jacobs", 
+            isCorrectAnswer: false, 
+            isSelected: false
+        }
+​​​​
+​​​​​
+answer: "Rin Tin Tin"
+​​​​​
+answerId: "hXo-t0NSu9PYLYdybz5M1"
+​​​​​
+isCorrectAnswer: false
+​​​​​
+isSelected: false
+​​​​​
+<prototype>: Object { … }
+​​​​
+2: Object { answerId: "_ISd__8Gqclf5G65spyUP", answer: "Chic Young", isCorrectAnswer: false, … }
+​​​​​
+answer: "Chic Young"
+​​​​​
+answerId: "_ISd__8Gqclf5G65spyUP"
+​​​​​
+isCorrectAnswer: false
+​​​​​
+isSelected: false
+​​​​​
+<prototype>: Object { … }
+​​​​
+3: Object { answerId: "x0rH5RmJn9xl07qK2Xi1q", answer: "HergÃ©", isCorrectAnswer: true, … }
+​​​​​
+answer: "HergÃ©"
+​​​​​
+answerId: "x0rH5RmJn9xl07qK2Xi1q"
+​​​​​
+isCorrectAnswer: true
+​​​​​
+isSelected: false
+​​​​​
+<prototype>: Object { … }
+​​​​
+length: 4
--- vite-project/src/Components/QuizPage.jsx
+++ vite-project/src/Components/QuizPage.jsx
@@ -19,8 +19,9 @@ export default function QuizPage(){
                     const correctAnswer = atob(result.correct_answer)
                     const incorrect_answers = result.incorrect_answers.map(answer => atob(answer))
                     const allAnswers = [...incorrect_answers, correctAnswer]
+                    const shuffledAnswers = shuffle(allAnswers)
                     
-                    const questionAndAnswerData = allAnswers.map(answer =>{
+                    const questionAndAnswerData = shuffledAnswers.map(answer =>{
                         const answerId = nanoid()
                         const isCorrectAnswer = (answer==correctAnswer ? true : false)
                         return(
@@ -32,87 +33,64 @@ export default function QuizPage(){
                             }
                         )
                     })
-                    console.log(questionAndAnswerData)
 
                     return(
                         {
                             question: question,
-                            answers: [questionAndAnswerData]
+                            answers: questionAndAnswerData
                         }
                     )
                 })
                 setQuizData(questionData)
-                console.log(quizData)
             })
         },[])
 
-            //         return({
-            //             ...result,
-            //             questionId: nanoid(),
-            //             question: atob(result.question),
-            //             allAnswers: [...decodedIncorrectAnswers, atob(result.correct_answer)],
-            //             correctAnswer:atob(result.correct_answer)
-            //         })
-            //     })
-                
-
-                
-            //     setQuizAnswers(answerData)
-            //     console.log(answerData)
-                           
-            // })
-            // .then(console.log(quizData))
-      
-
+ 
+    function shuffle(array){
+                for (let i=0; i<array.length; i++){
+                    const randomIndex = Math.floor(Math.random() * array.length)
+                    const currentArrayItem = array[i]
+                    // Replace the item at the current index location with the item from the random index location
+                    array[i] = array[randomIndex]
+                    // Move currentArrayItem to the random index location
+                    array[randomIndex] = currentArrayItem
+                }
+                return array
+    }
        
-//     const quizElements = quizData.map((quiz, index) => {
-//         const matchingQuizAnswers = quizAnswers[index]
-//         console.log(matchingQuizAnswers)
-//         const quizAnswerElements = matchingQuizAnswers.map(answer => {
-//             return(
-//                 <QuizAnswer
-//                     key={answer.answerId}
-//                     id={answer.answerId}
-//                     answer = {answer.answer}
-//                     isCorrectAnswer = {answer.isCorrectAnswer}
-//                     isSelected = {answer.isSelected}
-//                 />)
-//         })
+    const quizElements = quizData.map(singleQuestion => {
+        const answerArray = singleQuestion.answers
+
+        const quizAnswerElements = answerArray.map(answer => {
+            return(
+                <QuizAnswer
+                    key={answer.answerId}
+                    id={answer.answerId}
+                    answer = {answer.answer}
+                    isCorrectAnswer = {answer.isCorrectAnswer}
+                    isSelected = {answer.isSelected}
+                />)
+        })
         
-//         const shuffledAnswerElements = shuffle(quizAnswerElements)
+        return (
+            <div className="quizElement-container">
+                <QuizQuestion
+                    key = {nanoid()}
+                    question = {singleQuestion.question}
+                />
+                <div className="quizAnswer-container">
+                    {quizAnswerElements}
+                </div>
+            </div>
+        )
+        
+    }) 
 
-//         return (
-//             <div className="quizElement-container">
-//                 <QuizQuestion
-//                     key = {quiz.questionId}
-//                     question = {quiz.question}
-//                 />
-//                 <div className="quizAnswer-container">
-//                     {shuffledAnswerElements}
-//                 </div>
-//             </div>
-//         )
-//     })
-
-//     function shuffle(array){
-//         for (let i=0; i<array.length; i++){
-//             const randomIndex = Math.floor(Math.random() * array.length)
-//             const currentArrayItem = array[i]
-//             // Replace the item at the current index location with the item from the random index location
-//             array[i] = array[randomIndex]
-//             // Move currentArrayItem to the random index location
-//             array[randomIndex] = currentArrayItem
-//         }
-//         return array
-//     }
-
-    
-
-//     return(
-//         <div className="quizPage-container">
-//             {quizElements}
-//             <button className="quizPage-btn">Check answers</button>
-//         </div>
-//     )
-return(<div></div>)    
+    return(
+        <div className="quizPage-container">
+            {quizElements}
+            <button className="quizPage-btn">Check answers</button>
+        </div>
+    )
+ 
 }
