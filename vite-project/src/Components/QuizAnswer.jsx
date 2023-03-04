ab6d7384ba55d2f939546d89ac07e5e5ed6c4885
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-03-04 15:27:12 -0500

    Update QuizAnswer.jsx

--- vite-project/src/Components/QuizAnswer.jsx
+++ vite-project/src/Components/QuizAnswer.jsx
@@ -2,18 +2,18 @@ import React from "react"
 
 export default function QuizAnswer(props){
 
-        let hexCode = ""
-        if (props.hasBeenChecked){
-            if(props.isCorrectAnswer){
-                hexCode = "#94D7A2"
-            } else if (props.isSelected){
-                hexCode = "#F8BCBC"
-            }
-        } else if (props.isSelected){
-            hexCode = "#D6DBF5"
-        } else{
-            hexCode = "#F5F7FB"
+    let hexCode = ""
+    if (props.hasBeenChecked) {
+        if (props.isCorrectAnswer) {
+            hexCode = "#94D7A2"
+        } else if (props.isSelected) {
+            hexCode = "#F8BCBC"
         }
+    } else if (props.isSelected) {
+        hexCode = "#D6DBF5"
+    } else {
+        hexCode = "#F5F7FB"
+    }
     const styles={backgroundColor: hexCode}
 
     return(
