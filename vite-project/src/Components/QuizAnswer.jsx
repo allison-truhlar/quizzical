211abcacb6c1780d18483d5126d8dc2fbe9185af
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-03-04 11:02:50 -0500

    button color changes on clikc

--- vite-project/src/Components/QuizAnswer.jsx
+++ vite-project/src/Components/QuizAnswer.jsx
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function QuizAnswer(props){
 
-    const styles = {backgroundColor: props.isSelected ? "#D6DBF5" : "#F5F7FB"}
+    const styles = {backgroundColor: props.isSelected ? "#59E391" : "#FFFFFF"}
 
     return(
-        <button className="quizAnswer-btn" styles={styles} onClick={()=>props.selectAnswer(props.id)}>
+        <button className="quizAnswer-btn" style={styles} onClick={()=>props.selectAnswer(props.id)}>
         {props.answer}
         </button>   
     )
